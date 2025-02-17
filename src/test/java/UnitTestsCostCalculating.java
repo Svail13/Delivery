2c2254e55ddcc5d76e807d36f39773d9e5eb2300
svail <svail.13@gmail.com>
2025-02-17 15:54:58 +0300

    Added negative tests

--- src/test/java/UnitTestsCostCalculating.java
+++ src/test/java/UnitTestsCostCalculating.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
